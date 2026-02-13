46480dc00738ae472ec5b6b45e4b6c8833ee8ed5
fideljotojot <fideljotojot@gmail.com>
2026-02-13 23:43:22 +0800

    Add GitHub Actions workflow for deploying to GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: "/Trip_to_Italy/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
